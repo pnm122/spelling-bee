c1f9714b46ae29b7a51acdc416283fa9f26802c8
pnm122 <pnmartin02@gmail.com>
2024-04-04 01:04:15 -0400

    New word list

--- preprocessing/index.py
+++ preprocessing/index.py
@@ -1,28 +1,18 @@
-# https://github.com/dwyl/english-words/blob/master/words.txt
+## WORDLIST SOURCE: https://users.cs.duke.edu/~ola/ap/linuxwords
 
 import re
 import gzip
 from collections import defaultdict
 
-# Credit: Professor Lorraine Li at University of Pittsburgh
-# CS1671 Spring 2024 Assignment 2
-def load_ngram_counts(ngram_counts_file):
-    counts = defaultdict(int)
-    with gzip.open(ngram_counts_file, "rt", encoding="utf-8") as f:
-        for line in f:
-            token, count = line.strip().split("\t")
-            if token[0].islower():
-                counts[token] = int(count)
-    return counts
-
-def getMatchingWords(file: str, centerLetter: str, outsideLetters: str) -> list[str]:
+def get_matching_words(file: str, centerLetter: str, outsideLetters: str) -> list[str]:
   matches: list[str] = []
 
-  centerLetter = centerLetter.lower()
-  outsideLetters = outsideLetters.lower()
+  centerLetter = centerLetter.upper()
+  outsideLetters = outsideLetters.upper()
 
   with open(file, 'r') as f:
     for line in f.readlines():
+      line = line.upper()
       match = re.match(fr'[{outsideLetters}]*[{centerLetter}][{outsideLetters}]*\s', line)
 
       if not(match is None):
@@ -31,9 +21,7 @@ def getMatchingWords(file: str, centerLetter: str, outsideLetters: str) -> list[
     return matches
 
 # Filter words from input file into output file
-# Use ngram_counts to filter out words below a given threshold of commonness
-# Not perfect -- includes words that I wouldn't consider to be words but it's better than nothing
-def filterWords(inputFile: str, outputFile: str, ngram_counts: dict, filterThreshold: int):
+def filter_words(inputFile: str, outputFile: str):
   with open(inputFile, 'r') as f:
     output = open(outputFile, 'w')
     
@@ -43,8 +31,7 @@ def filterWords(inputFile: str, outputFile: str, ngram_counts: dict, filterThres
       # words that start with a capital letter (proper nouns)
       # words < 4 letters long
       match = re.match(r'[a-z][a-zA-Z]{3}[a-zA-z]*\s', line)
-      word = line.strip().lower()
-      counts = ngram_counts[word]
+      word = line.strip().upper()
 
       # Filter out words with > 7 different letters since I'm using this data
       # for a spelling bee game, where you only get 7 letters to choose from
@@ -57,24 +44,16 @@ def filterWords(inputFile: str, outputFile: str, ngram_counts: dict, filterThres
       
       # print(f'{word}: {numDifferentLetters}')
 
-      if not(match is None) and counts > filterThreshold and numDifferentLetters <= 7:
+      if not(match is None) and numDifferentLetters <= 7:
         output.write(f'{word}\n')
 
 if __name__ == '__main__':
-  matches = getMatchingWords(
-    'output.txt',
-    'A',
-    'TSRFIH'
-    # MANDOLIN
+  # filter_words('wordlist.txt', 'wordlist_filtered.txt')
+
+  matches = get_matching_words(
+    'wordlist_filtered.txt',
+    'N',
+    'UDERMI'
   )
 
   print(matches)
-
-  # ngram_counts = load_ngram_counts('ngram_counts.txt.gz')
-
-  # filterWords(
-  #   'words.txt',
-  #   'output.txt',
-  #   ngram_counts,
-  #   -1
-  # )
