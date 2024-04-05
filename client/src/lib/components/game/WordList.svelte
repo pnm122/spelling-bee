48ecb479bf25b8d81be414ad5ca86bfb19b76cea
pnm122 <pnmartin02@gmail.com>
2024-04-04 22:42:46 -0400

    Info on how points are calculated

--- client/src/lib/components/game/WordList.svelte
+++ client/src/lib/components/game/WordList.svelte
@@ -31,6 +31,20 @@
     </div>
     <div id="words-wrapper">
       <div id="words-found">
+        <button
+          on:click={() => openPopup(`
+            By default, the number of points you earn from a word is equal to the length of the word. For example,
+            finding the word "HELLO" will earn you 5 points. If you use a hint to find a word, you earn 1 point for each
+            letter the hint had not already given you. Finally, if you turn on word previews, all subsequent points are
+            reduced by 20%.
+          `)}
+          id="points-calculated-button"
+          aria-label="How are my points calculated?">
+          <PhInfo />
+          <span>
+            How are my points calculated?
+          </span>
+        </button>
         {#if $currentScore.data.wordsFound.length > 0}
           <!-- Must key the items because the words shift place in the array -->
           {#each $currentScore.data.wordsFound as data (data.word)}
@@ -74,6 +88,27 @@
     @media screen and (width > 768px) {
       --word-list-side-padding: 1.5rem;
     }
+  }
+
+  #points-calculated-button {
+    width: fit-content;
+    font: var(--label-sm);
+    color: var(--darkgray);
+    padding: 0.375rem 0.5rem;
+    transform: translateX(-0.5rem);
+    margin-bottom: 0.5rem;
+    border-radius: 0.25rem;
+    display: flex;
+    align-items: center;
+    gap: 0.25rem;
+    text-align: left;
+    /* Centers text with icon, maybe a better solution than this? */
+    line-height: 0;
+  }
+
+  #points-calculated-button:hover {
+    background-color: var(--gray);
+    color: var(--heading);
   }
 
   #word-list {
